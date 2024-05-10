f898ae5de618dac3cc0d2870e7ec118764fd2001
Quang Minh <quang07042008@gmail.com>
2024-05-10 23:48:21 +0700

    Minh

---
+++ webcheck/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webcheck.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+           
+            
+            
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username, string password)
+        {
+            string mess = "Login";
+            if(username.Equals("minh") && password.Equals("123"))
+            {
+                mess += " success!";
+            }
+            else
+            {
+                mess += "fail!";
+            }
+
+            ViewBag.message = mess;
+
+            return View();
+        }
+
+
+    }
+}
